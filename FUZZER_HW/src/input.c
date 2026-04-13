5276c7d6554575560699d87f04fb32c74653521c
AshLink95 <ashraf.zoubeh.123@gmail.com>
2026-04-12 20:55:34 -0400

    feat: implemented input, rendeting INPUT an opaque pointer, accessible through its API

--- FUZZER_HW/src/input.c
+++ FUZZER_HW/src/input.c
@@ -1,36 +1,48 @@
 #include "input.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct input {
+    char* str;
+    size_t len;
+    MUTATOR_STATE state;
+};
 
 INPUT make_input(const char *input_str) {
-    (void) input_str;
-    return 0;
+    INPUT input = malloc(sizeof *input);
+    input->len = strlen(input_str);
+    input->state = 0;
+    input->str = malloc(input->len + 1);
+    strcpy(input->str, input_str);
+    return input;
 }
 
 void free_input(INPUT input) {
-    (void) input;
+    free(input->str);
+    free(input);
 }
 
 size_t input_len(INPUT input) {
-    (void) input;
-    return 0;
+    return input->len;
 }
 
 const char *input_str(INPUT input) {
-    (void) input;
-    return 0;
+    return input->str;;
 }
 
 MUTATOR_STATE input_mutator_state(INPUT input) {
-    (void) input;
-    return 0;
+    return input->state;
 }
 
 MUTATOR_STATE input_set_state(INPUT input, MUTATOR_STATE state) {
-    (void) input;
-    (void) state;
-    return 0;
+    MUTATOR_STATE old_state = input->state;
+    input->state = state;
+    return old_state;
 }
 
 MUTATOR_STATE input_state_step(INPUT input) {
-    (void) input;
-    return 0;
+    MUTATOR_STATE old_state = input->state;
+    input->state++;
+    return old_state;
 }
