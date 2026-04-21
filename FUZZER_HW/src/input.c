bc665eccd995475eb0a67c6804087c20ea85b833
AshLink95 <ashraf.zoubeh.123@gmail.com>
2026-04-21 19:04:02 -0400

    ok ok

--- FUZZER_HW/src/input.c
+++ FUZZER_HW/src/input.c
@@ -10,6 +10,7 @@ struct input {
 };
 
 INPUT make_input(const char *input_str) {
+    if (!input_str) return NULL;
     INPUT input = malloc(sizeof(struct input));
     input->len = strlen(input_str);
     input->state = 0;
@@ -19,30 +20,35 @@ INPUT make_input(const char *input_str) {
 }
 
 void free_input(INPUT input) {
+    if (!input) return;
     free(input->str);
     free(input);
 }
 
 size_t input_len(INPUT input) {
+    if (!input) return 0;
     return input->len;
 }
 
 const char *input_str(INPUT input) {
-    return input->str;;
+    if (!input) return NULL;
+    return input->str;
 }
 
 MUTATOR_STATE input_mutator_state(INPUT input) {
+    if (!input) return 0;
     return input->state;
 }
 
 MUTATOR_STATE input_set_state(INPUT input, MUTATOR_STATE state) {
+    if (!input) return 0;
     MUTATOR_STATE old_state = input->state;
     input->state = state;
     return old_state;
 }
 
 MUTATOR_STATE input_state_step(INPUT input) {
-    MUTATOR_STATE old_state = input->state;
+    if (!input) return 0;
     input->state++;
-    return old_state;
+    return input->state;
 }
